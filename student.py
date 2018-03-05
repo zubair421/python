09179f230a97458636581fc6cf58ff2540ad574c
zubair421 <apj421@gmail.com>
2018-03-05 17:34:59 +0530

    Stud Started

---
+++ student.py
@@ -0,0 +1,7 @@
+name = input("Enter the name of the student: ")
+roll_no= input("Enter the rol no of the student: ")
+branch_name= input("Enter the branch of the student : ")
+marks= input("Enter vthe marks of the studetn: ")
+
+
+
